48de2e37ac24d84254b68ed1eef47320d600161a
Danylo <danyloboshagora@gmail.com>
2021-12-11 17:34:39 +0100

    staring on survey form

---
+++ src/app/survey-form/survey-form.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SurveyFormRoutingModule } from './survey-form-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SurveyFormRoutingModule
+  ]
+})
+export class SurveyFormModule { }
