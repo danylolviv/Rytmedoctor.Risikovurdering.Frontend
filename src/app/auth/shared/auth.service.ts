08fd077e286921d9a241e648a3728a79ee7908fa
Danylo <danyloboshagora@gmail.com>
2021-12-16 17:37:32 +0100

    accidentally starder working in the surveyForm branch, where as meant in the security

--- src/app/auth/shared/auth.service.ts
+++ src/app/auth/shared/auth.service.ts
@@ -6,6 +6,8 @@ import {TokenDto} from "./token.dto";
 import {environment} from "../../../environments/environment";
 import {take, tap} from "rxjs/operators";
 
+const jwtToken = "jwtToken";
+
 @Injectable({
   providedIn: 'root'
 })
@@ -17,11 +19,12 @@ export class AuthService {
 
   login(loginDto: LoginDto): Observable<TokenDto>{
     return this._http
+      // Angular HttpClient module is built so it will only "take(1), but there might be other 3rd party clients that do not do the same so you have to take care of possible memory leak"
       .post<TokenDto>(environment.api + "/api/Auth/Login", loginDto)
       .pipe(
         tap( token => {
           if (token && token.jwt){
-            localStorage.setItem('jwtToken', token.jwt)
+            localStorage.setItem(jwtToken, token.jwt)
             this.isLoggedIn$.next(token.jwt)
 
           }else{
@@ -32,11 +35,11 @@ export class AuthService {
   }
 
   getToken(): string | null {
-    return localStorage.getItem('jwtToken')
+    return localStorage.getItem(jwtToken)
   }
 
   logout(): Observable<boolean> {
-    localStorage.removeItem('jwtToken')
+    localStorage.removeItem(jwtToken)
     this.isLoggedIn$.next(null)
     return  of(true).pipe(take(1))
   }
