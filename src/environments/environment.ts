3b54d09497c2fcb7f7dd06f4f28e99b3e2954c67
Danylo <danyloboshagora@gmail.com>
2021-12-20 06:21:47 +0100

    api path new

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  api: "https://risikovurderinginteractiveform.azurewebsites.net"
+  api: "https://formbuildertutee.azurewebsites.net"
 };
 
 /*
